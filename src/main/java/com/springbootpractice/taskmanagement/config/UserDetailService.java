ed8c39368a50b29e92115e4390338b1e96b5aa7f
Adrian Nads Marcelo <marcelonads@gmail.com>
2024-08-31 18:38:22 +0800

    development: IMPLEMENTED UserDetailService

---
+++ src/main/java/com/springbootpractice/taskmanagement/config/UserDetailService.java
@@ -0,0 +1,18 @@
+package com.springbootpractice.taskmanagement.config;
+
+import com.springbootpractice.taskmanagement.authentication.AuthenticationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailService implements UserDetailsService {
+
+    @Autowired
+    private AuthenticationRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.repository.getUserByUsername(username).orElseThrow(() -> new RuntimeException("User Not Found"));
+    }
+}

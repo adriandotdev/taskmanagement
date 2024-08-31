512e9eab9e3d62b847b01c55e1b9254119b18f47
Adrian Nads Marcelo <marcelonads@gmail.com>
2024-08-31 13:01:53 +0800

    INITIAL COMMIT

---
+++ src/main/java/com/springbootpractice/taskmanagement/TaskmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.springbootpractice.taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class TaskmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagementApplication.class, args);
+	}
+}
